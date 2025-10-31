5c62389ce49e8659d7dd504bdab1fa53255231be
FelipeSousaP <comffelipe@gmail.com>
2025-10-31 10:54:11 -0300

    feat:teste

--- Projetos_de_Fisica/Pong/Assets/Little_Ball.cs
+++ Projetos_de_Fisica/Pong/Assets/Little_Ball.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class Little_Ball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [Header("Ball Settings")]
+    [SerializeField] Rigidbody rb;
     void Update()
     {
-        
+        // força = Direção, massa * aceleração
+        rb.AddForce(Vector3.forward * rb.mass, ForceMode.Acceleration);
     }
 }
