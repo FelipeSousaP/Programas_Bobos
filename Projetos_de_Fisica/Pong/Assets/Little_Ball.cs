71fd5e4f3b4c8361143e3f191956dfece56ddc1a
FelipeSousaP <comffelipe@gmail.com>
2025-10-31 11:29:12 -0300

    Fix: tentado fazer um codigo

--- Projetos_de_Fisica/Pong/Assets/Little_Ball.cs
+++ Projetos_de_Fisica/Pong/Assets/Little_Ball.cs
@@ -4,9 +4,15 @@ public class Little_Ball : MonoBehaviour
 {
     [Header("Ball Settings")]
     [SerializeField] Rigidbody rb;
+    [SerializeField] Vector3 VelocidadeInicial;
+
+    [Header("physics Setting")]
+    [SerializeField] float Gravidade;
+    public float time;
+
     void Update()
     {
-        // força = Direção, massa * aceleração
-        rb.AddForce(Vector3.forward * rb.mass, ForceMode.Acceleration);
+        time = Time.time;
+        transform.position += new Vector3(VelocidadeInicial.x,VelocidadeInicial.y,VelocidadeInicial.z) * Gravidade * time;
     }
 }
